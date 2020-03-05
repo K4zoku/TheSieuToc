2f4b92ca31e00ef05864b82614f6552d480b3375
takahatashun <takahatashunvn@gmail.com>
2020-03-05 23:03:33 +0700

    Update

--- src/main/java/me/lxc/artxeapi/utils/ArtxeChat.java
+++ src/main/java/me/lxc/artxeapi/utils/ArtxeChat.java
@@ -1,27 +1,21 @@
 package me.lxc.artxeapi.utils;
 
-import com.sun.istack.internal.NotNull;
-import com.sun.istack.internal.Nullable;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-
-import java.util.Arrays;
 
 public final class ArtxeChat {
 
-    public static TextComponent getTextComponent(@Nullable HoverEvent.Action hover, @Nullable BaseComponent[] hoverValue, @Nullable ClickEvent.Action click, @Nullable String clickValue, @NotNull String text){
+    public static TextComponent getTextComponent(HoverEvent.Action hover, BaseComponent[] hoverValue, ClickEvent.Action click, String clickValue, String text){
         TextComponent tc = new TextComponent(ChatColor.translateAlternateColorCodes('&', text));
         if(click != null && !clickValue.isEmpty()) {
             tc.setClickEvent(new ClickEvent(click, clickValue));
         }
         if(hover != null && hoverValue != null) {
-            tc.setHoverEvent(new HoverEvent(hover,hoverValue));
+            tc.setHoverEvent(new HoverEvent(hover, hoverValue));
         }
         return tc;
     }
